4a9994acadb375403e636ca844048a5bfbaf5ccd
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-03-10 17:12:28 +0100

    Add initial files

---
+++ Vagrantfile
@@ -0,0 +1,114 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require_relative 'provisioning/vbox.rb'
+VBoxUtils.check_version('7.2.4')
+Vagrant.require_version ">= 2.4.9"
+
+# Modifica la variable STUDENT_PREFIX para sustituir "X" por tu prefijo
+# Ejemplo, el alumno Roberto Rey Expósito, que hace la práctica en el curso
+# 25/26, utilizará el siguiente prefijo: rre2526
+STUDEN_PREFIX = "X"
+
+# Hostnames for master and worker nodes
+MASTER_HOSTNAME = "#{STUDEN_PREFIX}-master"
+WORKER_HOSTNAME = "#{STUDEN_PREFIX}-worker"
+
+# Master settings
+MASTER_IP = "192.168.56.10"
+MASTER_CORES = 1
+MASTER_MEM = 2048
+MASTER_NUM_DISKS=3
+MASTER_DISK_SIZE='4GB'
+# Worker settings
+NUM_WORKERS = 3
+WORKER_CORES = 1
+WORKER_MEM = 2048
+WORKER_NUM_DISKS=3
+WORKER_DISK_SIZE='8GB'
+
+require 'ipaddr'
+CLUSTER_IP_ADDR = IPAddr.new MASTER_IP
+CLUSTER_IP_ADDR = CLUSTER_IP_ADDR.succ
+
+ENV["VAGRANT_NO_PARALLEL"] = "yes"
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "rreye/debian-13"
+  config.vm.box_version = "20260304"
+  config.vm.box_check_update = false
+  config.vm.synced_folder ".", "/vagrant"
+
+  # Configure hostmanager plugin
+  config.hostmanager.enabled = true
+  config.hostmanager.manage_host = true
+  config.hostmanager.manage_guest = true
+
+  # Master node
+  config.vm.define "master", primary: true do |master|
+    master.vm.hostname = MASTER_HOSTNAME
+    master.vm.network "private_network", ip: MASTER_IP
+
+    for i in 1..MASTER_NUM_DISKS do
+        master.vm.disk :disk, size: MASTER_DISK_SIZE, primary: false, name: "disk#{i}"
+    end
+      
+    master.vm.provider "virtualbox" do |vb|
+        vb.name = "IAGE-#{master.vm.hostname}"
+        vb.customize ["modifyvm", :id, "--graphicscontroller", "vmsvga"]
+        vb.cpus = MASTER_CORES
+        vb.memory = MASTER_MEM
+        vb.gui = false
+    end
+    
+    master.vm.provider "libvirt" do |libv|
+        libv.title = "IAGE-#{master.vm.hostname}"
+        libv.driver = "kvm"
+        libv.cpus = MASTER_CORES
+        libv.memory = MASTER_MEM
+        libv.cputopology :sockets => '1', :cores => MASTER_CORES, :threads => '1'
+        
+        for i in 1..MASTER_NUM_DISKS do
+            libv.storage :file, :size => MASTER_DISK_SIZE
+        end
+    end
+  end
+  
+  # Worker nodes
+  (1..NUM_WORKERS).each do |i|
+    config.vm.define "worker#{i}" do |worker|
+	      worker.vm.hostname = "#{WORKER_HOSTNAME}#{i}"
+	      IP_ADDR = CLUSTER_IP_ADDR.to_s
+        CLUSTER_IP_ADDR = CLUSTER_IP_ADDR.succ
+        worker.vm.network "private_network", ip: IP_ADDR
+        
+        for i in 1..WORKER_NUM_DISKS do
+            worker.vm.disk :disk, size: WORKER_DISK_SIZE, primary: false, name: "disk#{i}"
+        end
+    
+        worker.vm.provider "virtualbox" do |vb|
+	          vb.name = "IAGE-#{worker.vm.hostname}"
+	          vb.customize ["modifyvm", :id, "--graphicscontroller", "vmsvga"]
+            vb.cpus = WORKER_CORES
+            vb.memory = WORKER_MEM
+	          vb.gui = false
+        end
+        
+        worker.vm.provider "libvirt" do |libv|
+            libv.title = "IAGE-#{worker.vm.hostname}"
+            libv.driver = "kvm"
+            libv.cpus = WORKER_CORES
+            libv.memory = WORKER_MEM
+            libv.cputopology :sockets => '1', :cores => WORKER_CORES, :threads => '1'
+            
+            for i in 1..WORKER_NUM_DISKS do
+                libv.storage :file, :size => WORKER_DISK_SIZE
+            end
+        end
+    end
+  end
+  
+  # Global provisioning bash script
+  config.vm.provision "global", type: "shell", run: "always", path: "provisioning/bootstrap.sh" do |script|
+      script.args = [MASTER_HOSTNAME]
+  end
+end
---
+++ provisioning/bootstrap.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$#" -ne 1 ]; then
+    echo "Sintaxis: $0 MASTER_HOSTNAME"
+    exit -1
+fi
+
+MASTER_HOSTNAME=$1
+
+if [ -b /dev/sdb ]; then
+    DISK0=/dev/sdb
+elif [ -b /dev/vdb ]; then
+    DISK0=/dev/vdb
+fi
+
+if [ -b /dev/sdc ]; then
+    DISK1=/dev/sdc
+elif [ -b /dev/vdc ]; then
+    DISK1=/dev/vdc
+fi
+
+if [ -b /dev/sdd ]; then
+    DISK2=/dev/sdd
+elif [ -b /dev/vdd ]; then
+    DISK2=/dev/vdd
+fi
+
+# Format and mount disks to be used with Hadoop HDFS and Spark
+if [ ! -d "/data/disk0" ]; then
+    mkdir -p /data/disk0 >& /dev/null
+    mkfs.ext4 -F $DISK0
+    mount $DISK0 /data/disk0
+    chmod 1777 /data/disk0
+else
+    if ! grep -Fq $DISK0 /proc/mounts ; then
+	mount $DISK0 /data/disk0 >& /dev/null
+	chmod 1777 /data/disk0
+    fi
+fi
+
+if [ ! -d "/data/disk1" ]; then
+    mkdir -p /data/disk1 >& /dev/null
+    mkfs.ext4 -F $DISK1
+    mount $DISK1 /data/disk1
+    chmod 1777 /data/disk1
+else
+    if ! grep -Fq $DISK1 /proc/mounts ; then
+	mount $DISK1 /data/disk1
+	chmod 1777 /data/disk1
+    fi
+fi
+
+if [ ! -d "/data/disk2" ]; then
+    mkdir -p /data/disk2 >& /dev/null
+    mkfs.ext4 -F $DISK2
+    mount /$DISK2 /data/disk2
+    chmod 1777 /data/disk2
+else
+    if ! grep -Fq $DISK2 /proc/mounts ; then
+	mount $DISK2 /data/disk2
+	chmod 1777 /data/disk2
+    fi
+fi
+
+if [ ! -d "/data/disk0/hdfs" ]; then
+    mkdir /data/disk0/hdfs
+fi
+
+if [ ! -d "/data/disk0/spark-tmp" ]; then
+    mkdir /data/disk0/spark-tmp
+fi
+
+if [ ! -d "/data/disk1/hdfs" ]; then
+    mkdir /data/disk1/hdfs
+fi
+
+if [ ! -d "/data/disk1/spark-tmp" ]; then
+    mkdir /data/disk1/spark-tmp
+fi
+
+if [ ! -d "/data/disk2/hdfs" ]; then
+    mkdir /data/disk2/hdfs
+fi
+
+if [ ! -d "/data/disk2/spark-tmp" ]; then
+    mkdir /data/disk2/spark-tmp
+fi
+
+chmod 1777 /data/disk0/hdfs
+chmod 1777 /data/disk1/hdfs
+chmod 1777 /data/disk2/hdfs
+chmod 1777 /data/disk0/spark-tmp
+chmod 1777 /data/disk1/spark-tmp
+chmod 1777 /data/disk2/spark-tmp
+
+if ! grep -Fq $DISK0 /etc/fstab ; then
+    echo -e "$DISK0        /data/disk0     ext4    defaults,relatime       0       0" >> /etc/fstab
+fi
+
+if ! grep -Fq $DISK1 /etc/fstab ; then
+    echo -e "$DISK1        /data/disk1     ext4    defaults,relatime       0       0" >> /etc/fstab
+fi
+
+if ! grep -Fq $DISK2 /etc/fstab ; then
+    echo -e "$DISK2        /data/disk2     ext4    defaults,relatime       0       0" >> /etc/fstab
+fi
+
+systemctl unmask systemd-timesyncd
+systemctl enable systemd-timesyncd.service
+systemctl restart systemd-timesyncd.service
+
+# Install software
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+apt-get update
+SOFTWARE="nano sshpass unzip python3-venv python-apt-common fdisk dnsutils dos2unix whois nfs-common openjdk-21-jdk systemd-timesyncd"
+echo "==> Installing software packages..."
+if ! apt-get install -y -qq $SOFTWARE > /tmp/apt.log 2>&1; then
+    echo "Error when installing software, log:"
+    cat /tmp/apt.log
+    exit 1
+fi
+echo "==> done"
+
+# .profile
+if ! grep -q "PATH=/sbin:\$PATH" /home/vagrant/.profile; then
+  echo 'export PATH=/sbin:$PATH' >> /home/vagrant/.profile
+fi
+
+# NFS and SSH keys setup
+SSH_PUBLIC_KEY=/share/.id_rsa.pub
+SSH_DIR=/home/vagrant/.ssh
+
+if [ ! -d "/share" ]; then
+    mkdir /share >& /dev/null
+fi
+
+if grep -Fq /share /etc/fstab ; then
+    sed -i "/share/d" /etc/fstab
+fi
+
+if [ "$(hostname)" = "$MASTER_HOSTNAME" ]; then
+    # Install NFS server
+    echo "==> Installing and configuring NFS server..."
+    if ! apt-get install -y -qq nfs-kernel-server >/tmp/apt.log 2>&1; then
+    	echo "Error when installing software, log:"
+    	cat /tmp/apt.log
+    	exit 1
+    fi
+    echo "==> done"
+
+    if [ ! -f $SSH_DIR/id_rsa.pub ]; then
+	# Create ssh keys
+	echo -e 'y\n' | sudo -u vagrant ssh-keygen -t rsa -f $SSH_DIR/id_rsa -q -N ''
+
+	if [ ! -f $SSH_DIR/id_rsa.pub ]; then
+		echo "SSH public key could not be created"
+		exit -1
+	fi
+    fi
+
+    if [ ! -f /etc/ssh/ssh_config.d/90-key-checking.conf ]; then
+        cat > /etc/ssh/ssh_config.d/90-key-checking.conf << EOF
+Host *
+    StrictHostKeyChecking no
+EOF
+    fi
+
+    chown vagrant:vagrant $SSH_DIR/id_rsa*
+    cp $SSH_DIR/id_rsa.pub $SSH_PUBLIC_KEY
+
+    # Configure NFS export
+    chmod 1777 /share
+    sed -i "/share/d" /etc/exports
+    echo -e "/share        192.168.56.0/24(rw,sync,no_subtree_check)" >> /etc/exports
+    exportfs -ra
+else
+    sed -i '/127.0.1.1.*-worker/d' /etc/hosts
+    umount /share >& /dev/null && sleep 2
+    if ! grep -Fq /share /etc/fstab ; then
+        echo -e "$MASTER_HOSTNAME:/share        /share     nfs    auto,relatime,tcp       0       0" >> /etc/fstab
+    fi
+    echo "Mounting NFS export"
+    sleep 2 && mount $MASTER_HOSTNAME:/share /share
+fi
+
+if [ ! -f $SSH_PUBLIC_KEY ]; then
+	echo "SSH public key does not exist"
+	exit -1
+fi
+
+sed -i '/127.0.1.1.*packer-amd64/d' /etc/hosts
+sed -i '/127.0.2.1/d' /etc/hosts
+sed -i "/master/d" $SSH_DIR/authorized_keys >& /dev/null
+cat $SSH_PUBLIC_KEY >> $SSH_DIR/authorized_keys
+chown vagrant:vagrant $SSH_DIR/authorized_keys
+chmod 0600 $SSH_DIR/authorized_keys
